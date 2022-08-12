bc51effa84bb5304fca49309a4a7e41a4ae49619
hedonismbot <anestezia06@mail.ru>
2022-08-12 17:56:34 +0300

    home assignment.1

---
+++ home_assignment/task6/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 7;
+if ((a % 2) == 0)
+{
+    Console.Write(a);
+    Console.WriteLine(" чётное?");
+    Console.WriteLine("да");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine(" чётное?");
+    Console.WriteLine("нет");
+}
+
---
+++ home_assignment/task8/Program.cs
@@ -0,0 +1,8 @@
+﻿int N = 8;
+int x = 2;
+while(x <= N)
+{
+    if((x % 2) == 0)
+    Console.WriteLine(x);
+    x = x + 2;
+}
