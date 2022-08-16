8b19fa6f7933dbf7ab8b15c1b9835c9ac17cf9b6
hedonismbot <anestezia06@mail.ru>
2022-08-16 22:08:31 +0300

    home assignment2

---
+++ home_assignment/task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число x: ");
+int x = int.Parse(Console.ReadLine());
+if(x < 100 && x > -100)
+Console.WriteLine("третьей цифры нет");
+else
+{
+    x = (x / 100) % 10;
+    Console.WriteLine("третья цифра числа - ");
+    Console.WriteLine(Math.Abs(x));
+}
---
+++ home_assignment/task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите день недели цифрой от 1 до 7: ");
+int x = int.Parse(Console.ReadLine());
+if(x<=7) 
+{
+    if(x<6)
+    Console.WriteLine("будний день, иди батрачить!");
+    else
+    Console.WriteLine("ура, выходной!");
+}
+else Console.WriteLine("На Земле в неделе только 7 дней, хитрюга");
