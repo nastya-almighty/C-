dd617161e90edd5d4d39cb7942fba601a62e158d
hedonismbot <anestezia06@mail.ru>
2022-08-16 17:25:22 +0300

    new functions

--- projects2/project21/obj/Debug/net6.0/project21.AssemblyInfo.cs
+++ projects2/project21/obj/Debug/net6.0/project21.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("project1_HelloConsole")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("project21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("project1_HelloConsole")]
-[assembly: System.Reflection.AssemblyTitleAttribute("project1_HelloConsole")]
+[assembly: System.Reflection.AssemblyProductAttribute("project21")]
+[assembly: System.Reflection.AssemblyTitleAttribute("project21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
