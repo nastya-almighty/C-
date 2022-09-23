9bc2a134afe2fa166cc58b6111d9a791ad79347a
hedonismbot <anestezia06@mail.ru>
2022-09-23 17:23:11 +0300

    home assignment 9

---
+++ home_assignment/task66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("задайте минимальное значение: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("задайте максимальное значение: ");
+int N = int.Parse(Console.ReadLine());
+
+int Sum (int MinValue, int MaxValue)
+{
+    int s = 0;
+    for (int i = MinValue; i <= MaxValue; i++)
+    {
+        s += i;
+    }
+    return s;
+}
+
+Console.Write("сумма натуральных элементов в промежутке от M до N - ");
+Console.WriteLine(Sum (M, N));
